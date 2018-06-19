ca558cf8386b6935f7e6566db5ededd0a532bfe5
arnoldazeem <arnoldazeem2008@yahoo.com>
2018-06-19 14:13:28 +0200

    house keeping

--- python/box/default.py
+++ python/box/default.py
@@ -1,22 +1,13 @@
-import numpy as np
-import matplotlib as mpl
 import glob
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-from pylab import *
-"""
-========
-Barchart
-========
-
-A bar plot with errorbars and height labels on individual bars
-"""
-import numpy as np
-import matplotlib.pyplot as plt
+from pandas.api.types import is_numeric_dtype
+import random
+import seaborn as sns
 
 
-path = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/bar/data/*.csv"
+path = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/box/data/*.csv"
 
 
 for fname in glob.glob(path):
@@ -25,82 +16,57 @@ for fname in glob.glob(path):
 
     filename, file_extension = os.path.splitext(graph_name)
 
+    # gets you only the numeric data
+    #df1 = df.select_dtypes(['number'])
     df1 = df._get_numeric_data()
+
+    listhem = []
+
+    listhem2 = []
+
     final = df1.columns[-1]
 
-    print(graph_name)
-
+    # gets you only the numeric data
+    # df1 = df.select_dtypes(['number'])
+    df1 = df._get_numeric_data()
 
     for a, b in enumerate(df1):
 
         if (df1.columns.values[a] != final):
-            #x_axis = df.iloc[:10, col_1].abs().values.tolist()
-            x_axis = df1.iloc[:5, a].values.tolist()
-            # make them positve
-            x_label = df1.columns[a]
 
-            y_axis = df1.iloc[:5, a+1].values.tolist()
-            # make them positve
-            y_label = df1.columns[a+1]
+            dfList = df1[b].tolist()
 
-            xn_axis = df1.iloc[6:10, a].values.tolist()
-            # make them positve
-            xn_label = df1.columns[a]
+            listhem.append(dfList[:17])
 
-            yn_axis = df1.iloc[6:10,a+1].values.tolist()
-            # make them positve
-            yn_label = df1.columns[a+1]
-            # plot
+            listhem2.append(dfList[18:38])
 
-            ## combine these different collections into a list
-            N = 5
-
-            men_std = (2, 3, 4, 1, 2)
-
-            women_std = (3, 5, 2, 3, 3)
-
-            ind = np.arange(N)  # the x locations for the groups
-            width = 0.35  # the width of the bars
-
-            fig, ax = plt.subplots()
-            rects1 = ax.bar(ind, x_axis, width, color='r', yerr=men_std)
+            # figure related code
+            fig = plt.figure()
+            #fig.suptitle('A notched box plot from' + filename, fontsize=10, fontweight='bold')
 
 
-            rects2 = ax.bar(ind + width, y_axis, width, color='y', yerr=women_std)
+            # Create an axes instance
+            ax = fig.add_subplot(111)
 
-            # add some text for labels, title and axes ticks
-            ax.set_ylabel('Scores')
-            ax.set_title('Example of barchart from ' + filename)
-            ax.set_xticks(ind + width / 2)
-            ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+            bp1 = ax.boxplot(listhem,1,'gD',  widths=0.35,
+                           patch_artist=True, boxprops=dict(facecolor="C0"))
+            bp2 = ax.boxplot(listhem2,1,'gD', widths=0.35,
+                             patch_artist=True, boxprops=dict(facecolor="C2"))
 
-            ax.legend((rects1[0], rects2[0]), (x_label, y_label))
+            ax.legend([bp1["boxes"][0], bp2["boxes"][0]], ['A', 'B'], loc='upper right')
 
+            # Create the boxplot
+            #ax.boxplot(listhem)
+            ax.set_title('A notched box plot from' + filename)
+            ax.set_xlabel('Distribution')
+            ax.set_ylabel('Value')
 
-            def autolabel(rects):
-                """
-                Attach a text label above each bar displaying its height
-                """
-                for rect in rects:
-                    height = rect.get_height()
-                    ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                            '%d' % int(height),
-                            ha='center', va='bottom')
+            #ax.set_xlim(0, 6)
 
-            print("here")
+            # Save the figure
+            fig.savefig('/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/box/'+'finaalnotches'+ df1.columns.values[a] + '.jpg', bbox_inches='tight')
 
-            autolabel(rects1)
-            autolabel(rects2)
-
-            plt.savefig(
-                '/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/bar/' + 'python2' +
-                df1.columns.values[a] + '.jpeg')
-
-            plt.show()
-
-        else:
-            print('done')
-            # end
+            fig.show()
 
 
 
--- python/box/rename script.py
+++ python/box/rename script.py
@@ -2,9 +2,9 @@ import os
 import glob
 
 
-path = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/bar/python bar dataset/*"
+path = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/lineplot/python line dataset/*"
 
-path2 = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/bar/python bar dataset/"
+path2 = "/home/adaboo/Desktop/Masters/sem4/thesis/plot_classification/python/lineplot/python line dataset/"
 
 
 for i,fname in enumerate (glob.glob(path)):
@@ -13,6 +13,6 @@ for i,fname in enumerate (glob.glob(path)):
 
     filename, file_extension = os.path.splitext(graph_name)
 
-    os.rename(path2 + graph_name, path2 +  "python_bar_chart" + "_" + str(i) + file_extension)
+    os.rename(path2 + graph_name, path2 +  "python_line_plot" + "_" + str(i) + file_extension)
 
     print("done")
